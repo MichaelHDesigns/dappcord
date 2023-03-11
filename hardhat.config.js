5dd63f39f9611e60fa5c29d769948b1c200938b7
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-11 18:14:48 -0500

    the server shall beg

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,63 @@
-require("@nomicfoundation/hardhat-toolbox");
+require ("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      chainId: 2330,
+      blockConfirmations: 1,
+      allowUnlimitedContractSize: true,
+    },
+    hardhat: {
+      chainId: 2330,
+      blockConfirmations: 1,
+      allowUnlimitedContractSize: true,
+    },
+
+
+    goerli: {
+      chainId: 2330,
+      blockConfirmations: 1,
+      url: 'https://rpc0.altcoinchain.org/rpc',
+      accounts: [process.env.PRIVATE_KEY],
+      allowUnlimitedContractSize: true,
+    },
+
+  },
+  solidity: {
+    compilers: [{ version: "0.8.4", settings: {
+      optimizer: {
+        runs: 200,
+        enabled: true
+      }
+    } }, { version: "0.8.17"}],
+  },
+
+  mocha: {
+    timeout: 300000, // 300 seconds max
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+
+  gasReporter: {
+    // enabled: process.env.REPORT_GAS !== undefined,
+    enabled: false,
+    currency: "INR",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+}

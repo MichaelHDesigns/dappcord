f4e3a03ae228491367add987ddac1cd0b7d1ee9f
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-18 01:07:44 -0400

    Update Servers.js

--- src/components/Servers.js
+++ src/components/Servers.js
@@ -2,46 +2,18 @@ import ethereum from '../assets/ethereum.svg';
 import plus from '../assets/plus.svg';
 import search from '../assets/search.svg';
 
-import { useState } from "react";
-import { ethers } from "ethers";
-import DappcordServer from "../abis/Dappcord.json";
-
-function Servers({ account, contract }) {
-  const [serverName, setServerName] = useState("");
-  const [serverPrice, setServerPrice] = useState("");
-
-  async function handleCreateServer() {
-    const price = ethers.utils.parseEther(serverPrice);
-    const tx = await contract.createServer(serverName, { value: price });
-    await tx.wait();
-    setServerName("");
-    setServerPrice("");
-  }
-
+const Servers = () => {
   return (
-    <div>
-      <h2>Create Server</h2>
-      <form onSubmit={handleCreateServer}>
-        <label>
-          Server Name:
-          <input
-            type="text"
-            value={serverName}
-            onChange={(e) => setServerName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Price (in ethers):
-          <input
-            type="text"
-            value={serverPrice}
-            onChange={(e) => setServerPrice(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Create Server</button>
-      </form>
+    <div className="servers">
+      <div className="server">
+        <img src={ethereum} alt="Ethereum Logo" />
+      </div>
+      <div className="server">
+        <img src={plus} alt="Add Server" />
+      </div>
+      <div className="server">
+        <img src={search} alt="Add Server" />
+      </div>
     </div>
   );
 }

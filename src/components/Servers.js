8b05bd774bb5e1290618895d71ac907bf9ee80e6
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-18 02:28:42 -0400

    Update Servers.js

--- src/components/Servers.js
+++ src/components/Servers.js
@@ -6,17 +6,17 @@ const Servers = () => {
   return (
   <div className="servers">
   <div className="server">
-    <a href="https://altcoinchain.org">
+    <a href="https://altcoinchain.org" target="_blank" rel="noopener noreferrer">
       <img src={ethereum} alt="Ethereum Logo" />
     </a>
   </div>
   <div className="server">
-    <a href="https://shop.mining.game/">
+    <a href="https://shop.mining.game/" target="_blank" rel="noopener noreferrer">
       <img src={plus} alt="Add Server" />
     </a>
   </div>
   <div className="server">
-    <a href="https://michaelh.org/">
+    <a href="https://michaelh.org/" target="_blank" rel="noopener noreferrer">
       <img src={search} alt="Search Servers" />
     </a>
   </div>

662d701e4e9bab5a19a2ee55e811f9069e9246b9
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-18 19:09:04 -0400

    Update Servers.js

--- src/components/Servers.js
+++ src/components/Servers.js
@@ -2,6 +2,7 @@ import ethereum from '../assets/ethereum.png';
 import plus from '../assets/plus.png';
 import search from '../assets/search.jpg';
 import market from '../assets/market.png';
+import swap from '../assets/swap.png';
 
 const Servers = () => {
   return (
@@ -12,7 +13,7 @@ const Servers = () => {
     </a>
   </div>
 <div className="server">
-    <a href="http://206.189.231.104:4000/" target="_blank" rel="noopener noreferrer">
+    <a href="http://altcord.com:4000/" target="_blank" rel="noopener noreferrer">
       <img src={market} alt="Market Logo" />
     </a>
   </div>
@@ -24,6 +25,11 @@ const Servers = () => {
   <div className="server">
     <a href="https://michaelh.org/" target="_blank" rel="noopener noreferrer">
       <img src={search} alt="Search Servers" />
+    </a>
+  </div>
+ <div className="server">
+    <a href="https://swapin.co/dex/" target="_blank" rel="noopener noreferrer">
+      <img src={swap} alt="Swap Servers" />
     </a>
   </div>
 </div>

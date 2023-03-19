cfc992ce2c9d83b447f4a40a0b237eabcfc3573d
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-19 00:34:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const PORT = process.env.PORT || 3030;
-const server = app.listen(PORT, () => console.log(`Listening on ${PORT}\n`));
+const PORT = process.env.PORT || 3030
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}\n`))
 
-const users = [];
-const messages = [];
+const messages = [
+  {
+    channel: "1",
+    account: "0x8dA3dA022d7a5224615c8F2E3fFdDc5B883B24A2",
+    text: "Welcome to Altcord!"
+  },
+  {
+    channel: "2",
+    account: "0x8dA3dA022d7a5224615c8F2E3fFdDc5B883B24A2",
+    text: "Welcome to the Market Channel! Here you can talk about the market!"
+  },
+]
 
 const { Server } = require("socket.io");
 const io = new Server(server, {
@@ -17,21 +27,12 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('a user connected')
 
-  socket.on('get users', () => {
-    io.emit('get users', users);
-  })
-
-  socket.on('new user', (user) => {
-    users.push(user);
-    io.emit('new user', user);
-  })
-
   socket.on('get messages', () => {
-    io.emit('get messages', messages);
+    io.emit('get messages', messages)
   })
 
-  socket.on('new message', (message) => {
-    messages.push(message);
-    io.emit('new message', message);
+  socket.on('new message', (msg) => {
+    messages.push(msg)
+    io.emit('new message', messages)
   })
-});
+})
